6ce4d5c0a979a2fd27cc6ae9c01b00992f84566d
Federico <federleal@gmail.com>
2025-05-02 09:55:42 +0100

    install maven script modified to a more stable version

--- .github/install_maven.ps1
+++ .github/install_maven.ps1
@@ -2,6 +2,15 @@ param (
     [string]$sourcesDirectory
 )
 
+Write-Host "Installing Java..."
+$javaDownloadUrl = "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.6+10/OpenJDK17U-jdk_x64_windows_hotspot_17.0.6_10.msi"
+$javaInstallerPath = "$sourcesDirectory\OpenJDK17U-jdk_x64_windows_hotspot_17.0.6_10.msi"
+
+Invoke-WebRequest -Uri $javaDownloadUrl -OutFile $javaInstallerPath
+
+Write-Host "Installing Java..."
+Start-Process -FilePath $javaInstallerPath -ArgumentList "/qn" -Wait
+
 Write-Host "Installing Maven..."
 $downloadUrl = "https://archive.apache.org/dist/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip"
 $destinationPath = "$sourcesDirectory\Maven"
@@ -16,6 +25,8 @@ Write-Host "Extracting Maven..."
 Expand-Archive -Path "$destinationPath\apache-maven-3.8.6-bin.zip" -DestinationPath "$destinationPath" -Force
 
 Write-Host "Setting environment variables..."
+$env:JAVA_HOME = "C:\Program Files\AdoptOpenJDK\jdk-17.0.6+10"
+$env:PATH += ";$env:JAVA_HOME\bin"
 $env:PATH += ";$destinationPath\apache-maven-3.8.6\bin"
 
 Write-Host "Verifying Maven installation..."
