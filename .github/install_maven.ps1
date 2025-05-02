ccef61895a5d433684deb98a9567b309c2a2533d
Federico <federleal@gmail.com>
2025-05-02 09:37:49 +0100

    install maven script modified to a more stable version

--- .github/install_maven.ps1
+++ .github/install_maven.ps1
@@ -1,5 +1,14 @@
-# Download and install Maven
-Invoke-WebRequest -Uri "https://dl.bintray.com/apache/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip" -OutFile "C:\Maven\apache-maven-3.8.6-bin.zip"
-Expand-Archive -Path "C:\Maven\apache-maven-3.8.6-bin.zip" -DestinationPath "C:\Maven"
-# Add Maven to the system's PATH environment variable
-$env:PATH += ";C:\Maven\bin"
+$downloadUrl = "https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip"
+$destinationPath = "C:\Maven"
+
+# Download Maven
+Invoke-WebRequest -Uri $downloadUrl -OutFile "$destinationPath\apache-maven-3.8.6-bin.zip"
+
+# Extract Maven
+Expand-Archive -Path "$destinationPath\apache-maven-3.8.6-bin.zip" -DestinationPath "$destinationPath" -Force
+
+# Set environment variables
+$env:PATH += ";$destinationPath\apache-maven-3.8.6\bin"
+
+# Verify Maven installation
+mvn --version
