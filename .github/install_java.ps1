7bf226afc0539f6b015ff3212437359f16be522b
Federico <federleal@gmail.com>
2025-05-02 10:03:52 +0100

    install maven script modified to a more stable version

---
+++ .github/install_java.ps1
@@ -0,0 +1,22 @@
+param (
+    [string]$sourcesDirectory
+)
+
+Write-Host "Installing Maven..."
+$downloadUrl = "https://archive.apache.org/dist/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip"
+$destinationPath = "$sourcesDirectory\Maven"
+
+Write-Host "Creating Maven directory..."
+New-Item -ItemType Directory -Path $destinationPath -Force
+
+Write-Host "Downloading Maven..."
+Invoke-WebRequest -Uri $downloadUrl -OutFile "$destinationPath\apache-maven-3.8.6-bin.zip"
+
+Write-Host "Extracting Maven..."
+Expand-Archive -Path "$destinationPath\apache-maven-3.8.6-bin.zip" -DestinationPath "$destinationPath" -Force
+
+Write-Host "Setting environment variables..."
+$env:PATH += ";$destinationPath\apache-maven-3.8.6\bin"
+
+Write-Host "Verifying Maven installation..."
+mvn --version
