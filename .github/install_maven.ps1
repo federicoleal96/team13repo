372c939192e2b8a7a305138dd25da1b70395d4ff
Federico <federleal@gmail.com>
2025-05-02 09:51:48 +0100

    install maven script modified to a more stable version

--- .github/install_maven.ps1
+++ .github/install_maven.ps1
@@ -2,20 +2,21 @@ param (
     [string]$sourcesDirectory
 )
 
-$downloadUrl = "https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip"
+Write-Host "Installing Maven..."
+$downloadUrl = "https://archive.apache.org/dist/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip"
 $destinationPath = "$sourcesDirectory\Maven"
 
-# Create the Maven directory if it doesn't exist
+Write-Host "Creating Maven directory..."
 New-Item -ItemType Directory -Path $destinationPath -Force
 
-# Download Maven
-Invoke-WebRequest -Uri "https://archive.apache.org/dist/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip" -OutFile "$destinationPath\apache-maven-3.8.6-bin.zip"
+Write-Host "Downloading Maven..."
+Invoke-WebRequest -Uri $downloadUrl -OutFile "$destinationPath\apache-maven-3.8.6-bin.zip"
 
-# Extract Maven
+Write-Host "Extracting Maven..."
 Expand-Archive -Path "$destinationPath\apache-maven-3.8.6-bin.zip" -DestinationPath "$destinationPath" -Force
 
-# Set environment variables
+Write-Host "Setting environment variables..."
 $env:PATH += ";$destinationPath\apache-maven-3.8.6\bin"
 
-# Verify Maven installation
+Write-Host "Verifying Maven installation..."
 mvn --version
